876231b042f798655f7dba1353e5d590313c537d
Neville Muli <neville.muli@gmail.com>
2025-05-22 19:06:35 +0300

    finishes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+        self.sorted_lower = sorted(word.lower())
+    
+    def match(self, candidates):
+        original_lower = self.word.lower()
+        anagrams = []
+        for candidate in candidates:
+            candidate_lower = candidate.lower()
+            if candidate_lower == original_lower:
+                continue
+            if sorted(candidate_lower) == self.sorted_lower:
+                anagrams.append(candidate)
+        return anagrams
